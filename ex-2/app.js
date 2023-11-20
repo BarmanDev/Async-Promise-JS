18f0a37cbdfc7fd3f5dbfdf6f27bc7efb23cef5b
Borja <elbarmandev@gmail.com>
2023-11-20 18:31:42 +0100

    ex2 promise finish

--- ex-2/app.js
+++ ex-2/app.js
@@ -1,46 +1,40 @@
-let data = {
-    "results": [
-        {
-            "title": "Estudiar Js",
-            "priority": "alta",
-            "isDone": false
-        },
-        {
-            "title": "Estudiar CSS",
-            "priority": "alta",
-            "isDone": true
-        },
-        {
-            "title": "Estudiar OOP",
-            "priority": "media",
-            "isDone": false
-        },
-        {
-            "title": "Estudiar IA",
-            "priority": "baja",
-            "isDone": false
-        }
-    ]
-};
-
-
-function dataConsole(){
-data.results.forEach(function(item) {
-    console.log(item.title);
-});
-
-}
-
 function buttonTitles() {
-    let titleList = document.getElementById('title-list');
-    titleList.innerHTML = '';
-    data.results.forEach(function(item) {
-        let listItem = document.createElement('h2'); 
-        listItem.textContent = item.title; 
-        titleList.appendChild(listItem); 
-    });
+    fetch('list.json')
+        .then(response => response.json())
+        .then(data => {
+            let titleList = document.getElementById('title-list');
+            let table = document.createElement('table');
+            let tr = document.createElement('tr');
+            let th1 = document.createElement('th');
+            let th2 = document.createElement('th');
+            let th3 = document.createElement('th');
+
+            th1.textContent = 'Título';
+            th2.textContent = 'Prioridad';
+            th3.textContent = '¿Hecho?';
+
+            tr.appendChild(th1);
+            tr.appendChild(th2);
+            tr.appendChild(th3);
+            table.appendChild(tr);
+
+            data.results.forEach(item => {
+                let tr = document.createElement('tr');
+                let td1 = document.createElement('td');
+                let td2 = document.createElement('td');
+                let td3 = document.createElement('td');
+
+                td1.textContent = item.title;
+                td2.textContent = item.priority;
+                td3.textContent = item.isDone ? 'Sí' : 'No';
+
+                tr.appendChild(td1);
+                tr.appendChild(td2);
+                tr.appendChild(td3);
+                table.appendChild(tr);
+            });
+
+            titleList.appendChild(table);
+        })
+    .catch(error => console.error(' error encontrado snif snif :( ', error));
 }
-
-
-
-dataConsole();
